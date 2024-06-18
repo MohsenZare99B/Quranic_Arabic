28fb389921a9c9acd1b2f4029d411933554acbbe
mohsenzareb <mohsenzare99b@gmail.com>
2024-06-18 21:12:20 +0330

    changed

--- quran_streamlit.py
+++ quran_streamlit.py
@@ -53,7 +53,7 @@ def display_verses_and_translations(iter_range=5):
         st.markdown(translations, unsafe_allow_html=True)
         st.write("---")  # Separator between verses
 
-st.header("Choose a random verse from the Quran")
+st.header("Choose a random verse")
 # Layout for input fields in the same row
 col1, col2, col3 = st.columns(3)
 
@@ -76,7 +76,7 @@ if st.button("Choose a random verse", key="generate_1"):
 display_text_and_translation()
 st.markdown("---")
 
-st.header("Select verses from the Quran")
+st.header("Select verses")
 # Layout for input fields in the same row
 col1, col2, col3, col4 = st.columns(4)
 
